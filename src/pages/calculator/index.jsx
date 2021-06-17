89e6736e149f51b4da0b423cd0775d6c3a8b8588
Eduardo Drumond <drumond2803@gmail.com>
2021-06-17 15:03:22 -0300

    Create raw template of calculator

---
+++ src/pages/calculator/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+    render() {
+        return (
+        <div>
+            <p>Here we gonna show the value</p>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+            <button>,</button>
+            <button>+</button>
+            <button>-</button>
+            <button>÷</button>
+            <button>x</button>
+            <button>=</button>
+            <button>%</button>
+            <button>√</button>
+        </div>
+        )
+    }
+}
+
+export default Calculator;
